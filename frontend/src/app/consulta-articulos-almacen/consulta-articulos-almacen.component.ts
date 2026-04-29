f8706ce06e567e8b584325a351af81d9544e26f0
zaid03 <terguyzaid@gmail.com>
2026-04-29 19:04:33 +0100

    ui for table + search and main buttons

--- frontend/src/app/consulta-articulos-almacen/consulta-articulos-almacen.component.ts
+++ frontend/src/app/consulta-articulos-almacen/consulta-articulos-almacen.component.ts
@@ -1,11 +1,156 @@
-import { Component } from '@angular/core';
+import { Component, HostListener} from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule, JsonPipe } from '@angular/common';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
+import { environment } from '../../environments/environment';
+import * as XLSX from 'xlsx';
+import { saveAs } from 'file-saver';
 @Component({
   selector: 'app-consulta-articulos-almacen',
-  imports: [],
+  standalone: true,
+  imports: [ CommonModule ,FormsModule, SidebarComponent],
   templateUrl: './consulta-articulos-almacen.component.html',
-  styleUrl: './consulta-articulos-almacen.component.css'
+  styleUrls: ['./consulta-articulos-almacen.component.css']
 })
 export class ConsultaArticulosAlmacenComponent {
+  //3 dots menu 
+  showMenu = false;
+  toggleMenu(event: MouseEvent): void {
+    event.stopPropagation();
+    this.showMenu = !this.showMenu;
+  }
 
+  @HostListener('document:click')
+  closeMenu(): void {
+    this.showMenu = false;
+  }
+
+  //global variables
+  private entcod: number | null = null;
+  private eje: number | null = null;
+  almacenes: any[] = [];
+  page = 0;
+  pageSize = 20;
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  isLoading: boolean = false;
+  almacenSuccess: string = '';
+  almacenError: string = '';
+  ngOnInit(): void{
+    this.limpiarMessages();
+    const entidad = sessionStorage.getItem('Entidad');
+    const eje = sessionStorage.getItem('EJERCICIO'); 
+
+    if (entidad) {const parsed = JSON.parse(entidad); this.entcod = parsed.ENTCOD;}
+    if (eje) {const parsed = JSON.parse(eje); this.eje = parsed.eje;}
+
+    if (!entidad || this.entcod === null || !eje || this.eje === null) {
+      sessionStorage.clear();
+      alert('Debes iniciar sesión para acceder a esta página.');
+      this.router.navigate(['/login']);
+      return;
+    }
+
+    this.fetchAlmacenes();
+  }
+
+  fetchAlmacenes() {
+    // this.isLoading = true;
+    this.limpiarMessages();
+
+
+  }
+  private updatePagination(): void {const total = this.totalPages;
+    if (total === 0) {this.page = 0; return;}
+  }
+  get paginatedAlmacen(): any[] {return this.almacenes || [];}
+  get totalPages(): number {return Math.max(1, Math.ceil((this.almacenes?.length ?? 0) / this.pageSize));}
+  prevPage() {
+    if (this.page == 0) return;
+    this.page = this.page - 1;
+    this.fetchAlmacenes() //will be replaced later
+    // this.isSearching ? this.searchArticulo() : this.fetchArticulos();
+    return;
+  }
+  nextPage() {
+    this.page = this.page + 1;
+    this.fetchAlmacenes() //will be replaced later
+    // this.isSearching ? this.searchArticulo() : this.fetchArticulos();
+    return;
+  }
+
+  //main table functions
+  sortField: 'afacod' | 'asucod' | 'artcod' | 'artdes' | 'artuni' | 'artref' | 'artblo' | 'afades' | null = null;
+  sortColumn: string = '';
+  sortDirection: 'asc' | 'desc' = 'asc';
+  toggleSort(column: string) {
+    if (this.sortColumn === column) {
+      this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+    } else {
+      this.sortColumn = column;
+      this.sortDirection = 'asc';
+    }
+    this.applySort();
+    this.page = 0;
+    this.updatePagination();
+  }
+
+  private applySort(): void {
+    if (!this.sortColumn) return;
+    
+    const parseValue = (val: string) => {
+      return val.replace(/(\d+)/g, (match) => {
+        return match.padStart(20, '0');
+      });
+    };
+
+    this.almacenes.sort((a, b) => {
+      const aValue = parseValue((a[this.sortColumn] ?? '').toString().toUpperCase());
+      const bValue = parseValue((b[this.sortColumn] ?? '').toString().toUpperCase());
+      
+      const comparison = aValue.localeCompare(bValue);
+      return this.sortDirection === 'asc' ? comparison : -comparison;
+    });
+  }
+
+  private startX: number = 0;
+  private startWidth: number = 0;
+  private resizingColIndex: number | null = null;
+  startResize(event: MouseEvent, colIndex: number) {
+    this.resizingColIndex = colIndex;
+    this.startX = event.pageX;
+    const th = (event.target as HTMLElement).parentElement as HTMLElement;
+    this.startWidth = th.offsetWidth;
+
+    document.addEventListener('mousemove', this.onResizeMove);
+    document.addEventListener('mouseup', this.stopResize);
+  }
+
+  onResizeMove = (event: MouseEvent) => {
+    if (this.resizingColIndex === null) return;
+    const table = document.querySelector('.main-table') as HTMLTableElement;
+    if (!table) return;
+    const th = table.querySelectorAll('th')[this.resizingColIndex] as HTMLElement;
+    if (!th) return;
+    const diff = event.pageX - this.startX;
+    th.style.width = (this.startWidth + diff) + 'px';
+  };
+
+  stopResize = () => {
+    document.removeEventListener('mousemove', this.onResizeMove);
+    document.removeEventListener('mouseup', this.stopResize);
+    this.resizingColIndex = null;
+  };
+
+  //misc
+  limpiarMessages() {
+    this.almacenSuccess = '';
+    this.almacenError = '';
+  }
 }
